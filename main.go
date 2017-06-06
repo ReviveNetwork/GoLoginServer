e1ae4513ea56999a4fa642a620228a9524862654
Max Bosse <max@bosse.io>
2017-06-06 15:33:28 +0200

    First searchProvider commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+
+	core "github.com/ReviveNetwork/GoRevive/Core"
+	log "github.com/ReviveNetwork/GoRevive/Log"
+)
+
+var (
+	// BuildTime of the build provided by the build command
+	BuildTime = "Not provided"
+	// GitHash of build provided by the build command
+	GitHash = "Not provided"
+	// GitBranch of the build provided by the build command
+	GitBranch = "Not provided"
+	// compileVersion we are receiving by the build command
+	CompileVersion = "0"
+	// Version of the Application
+	Version = "0.0.0"
+
+	// MyConfig Default configuration
+	MyConfig = Config{
+		MysqlServer: "localhost:3306",
+		MysqlUser:   "loginserver",
+		MysqlDb:     "loginserver",
+		MysqlPw:     "",
+	}
+)
+
+func main() {
+	var (
+		configPath = flag.String("config", "config.yml", "Path to yml configuration file")
+		logLevel   = flag.String("loglevel", "error", "LogLevel [error|warning|note|debug]")
+	)
+	flag.Parse()
+
+	if CompileVersion != "0" {
+		Version = Version + "." + CompileVersion
+	}
+
+	log.SetLevel(*logLevel)
+	log.Notef("Starting up v%s", Version)
+
+	MyConfig.Load(*configPath)
+	// Startup done
+
+	dbConnection := new(core.DB)
+	dbSQL, err := dbConnection.New(MyConfig.MysqlServer, MyConfig.MysqlDb, MyConfig.MysqlUser, MyConfig.MysqlPw)
+	if err != nil {
+		log.Fatalln("Error connecting to DB", err)
+	}
+
+	searchProvider := new(SearchProvider)
+	searchProvider.New("SP", dbSQL)
+}
---
+++ searchProvider.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"database/sql"
+	"strconv"
+
+	gs "github.com/ReviveNetwork/GoRevive/GameSpy"
+	log "github.com/ReviveNetwork/GoRevive/Log"
+)
+
+type SearchProvider struct {
+	name          string
+	socket        *gs.Socket
+	eventsChannel chan gs.SocketEvent
+	db            *sql.DB
+}
+
+// New creates and starts a new SearchProvider
+func (sP *SearchProvider) New(name string, db *sql.DB) {
+	var err error
+
+	sP.socket = new(gs.Socket)
+	sP.name = name
+	sP.db = db
+	sP.eventsChannel, err = sP.socket.New(sP.name, "29901")
+
+	if err != nil {
+		log.Errorln(err)
+	}
+
+	go sP.run()
+}
+
+func (sP *SearchProvider) run() {
+	for {
+		select {
+		case event := <-sP.eventsChannel:
+			switch {
+			case event.Name == "client.command.nicks":
+				sP.nicks(event.Data.(gs.EventClientCommand))
+			case event.Name == "client.command.check":
+				sP.check(event.Data.(gs.EventClientCommand))
+			default:
+				log.Debugln(event)
+			}
+		}
+	}
+}
+
+func (sP *SearchProvider) check(event gs.EventClientCommand) {
+	nick, okNick := event.Command.Message["nick"]
+	if !okNick {
+		event.Client.WriteError("0", "Invalid query!")
+		return
+	}
+
+	log.Noteln("Check\t" + nick)
+
+	if !event.Client.IsActive {
+		log.Noteln("Client left")
+		return
+	}
+
+	stmt, err := sP.db.Prepare("SELECT pid FROM revive_soldiers WHERE nickname=? AND game=?")
+	if err != nil {
+		event.Client.WriteError("0", "The login service is having an issue reaching the database. Please try again in a few minutes.")
+		return
+	}
+
+	var pid int
+
+	err = stmt.QueryRow(nick, "battlefield2").Scan(pid)
+	if err != nil {
+		event.Client.WriteError("256", "Invalid username. Account does not exist!")
+		return
+	}
+
+	event.Client.Write("\\cur\\0\\pid\\" + strconv.Itoa(pid) + "\\final\\")
+}
+
+func (sP *SearchProvider) nicks(event gs.EventClientCommand) {
+	var passMD5 string
+
+	email, okEmail := event.Command.Message["email"]
+	pass, okPass := event.Command.Message["pass"]
+	passenc, okPassenc := event.Command.Message["passenc"]
+	if !okEmail || (!okPass && !okPassenc) {
+		event.Client.WriteError("0", "Invalid query!")
+		return
+	}
+
+	if okPass {
+		passMD5 = gs.Hash(pass)
+	} else if okPassenc {
+		decodedPass, err := gs.DecodePassword(passenc)
+		if err != nil {
+			event.Client.WriteError("0", "Invalid password!")
+			return
+		}
+		passMD5 = gs.Hash(decodedPass)
+	}
+
+	log.Noteln("Nicks\t" + email)
+
+	if !event.Client.IsActive {
+		log.Noteln("Client left")
+		return
+	}
+
+	stmt, err := sP.db.Prepare("SELECT username FROM web_users WHERE email=? AND password=?")
+	if err != nil {
+		event.Client.WriteError("0", "The login service is having an issue reaching the database. Please try again in a few minutes.")
+		return
+	}
+
+	rows, err := stmt.Query(email, passMD5)
+	if err != nil {
+		event.Client.WriteError("0", "The login service is having an issue reaching the database. Please try again in a few minutes.")
+		return
+	}
+
+	var usernames []string
+	for rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			event.Client.WriteError("0", "The login service is having an issue reaching the database. Please try again in a few minutes.")
+			return
+		}
+		usernames = append(usernames, username)
+	}
+
+	var out = "\\nr\\" + strconv.Itoa(len(usernames))
+
+	for _, user := range usernames {
+		out = out + "\\nick\\" + user + "\\uniquenick\\" + user
+	}
+
+	out = out + "\\ndone\\final\\"
+	event.Client.Write(out)
+}

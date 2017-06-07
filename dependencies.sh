7da9dca1208026ba351054ee0d53a88198ee305b
Spencer <spencer@sf-n.com>
2017-06-07 00:30:21 -0400

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,1 +1,1 @@
-ssh-agent bash -c 'ssh-add /home/jenkins/.ssh/id_rsa; git clone git@github.com:ReviveNetwork/GoRevive.git $GOPATH/src/github.com/GoRevive'
+git clone git@github.com:ReviveNetwork/GoRevive.git $GOPATH/src/github.com/ReviveNetwork/GoRevive

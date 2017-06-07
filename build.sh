76961588f29e62b14709ffde7bfda8c59626b283
Spencer <spencer@sf-n.com>
2017-06-07 00:39:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "Preparing Project Environment"
 export GOPATH=$WORKSPACE/gopath
 export GITHUBPATH=$GOPATH/src/github.com
+mkdir -p $GOPATH
 
 echo "Cloning Project Dependencies"
 git clone git@github.com:ReviveNetwork/GoRevive.git $GITHUBPATH/ReviveNetwork/GoRevive
